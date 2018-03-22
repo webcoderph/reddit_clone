2bc267ba869def47ff55f04ef4edf0f1f2e957d3
webcoderph <webcoderph@gmail.com>
2018-03-22 13:01:21 +0800

    add type of posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322040138) do
+ActiveRecord::Schema.define(version: 20180322050008) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180322040138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "text"
+    t.string "type"
     t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
