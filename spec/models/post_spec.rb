55417b877a6ad1148fa506d88535ac1589a84f84
webcoderph <webcoderph@gmail.com>
2018-03-27 16:39:10 +0800

    add sidekiq and redis

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 describe Post do
-  it { is_expected.to validate_presence_of(:url)}
+  it { is_expected.to validate_presence_of(:url) }
+  it { is_expected.to validate_presence_of(:type) }
 end
